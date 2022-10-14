5c83c44d6f6472183e2b6ba639ecf32ba933f701
MathieuDacheux <mathieu.dacheux@icloud.com>
2022-10-14 14:08:31 +0200

    Features

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -1,11 +1,13 @@
 <?php
 
-// Variables
-
 // Appel des configurations
 
 // Appel des fonctions
 
+// Variables
+
 // Appel des vues
 
-
+include(__DIR__.'/../views/templates/header.php');
+include(__DIR__.'/../views/home.php');
+include(__DIR__.'/../views/templates/footer.php');
